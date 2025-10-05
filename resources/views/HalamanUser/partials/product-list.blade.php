a53239b852c2af363e9c12cccc3e0af190e2b2cf
AHMADDWI007 <ahmaddwikurnia23@mhs.politala.ac.id>
2025-10-05 18:41:41 +0800

    merapikan seluruh halaman user beserta javascriptnya dan mengatasi checkout

--- resources/views/HalamanUser/partials/product-list.blade.php
+++ resources/views/HalamanUser/partials/product-list.blade.php
@@ -2,11 +2,18 @@
 <div class="col-lg-4 col-md-6 text-center">
   <div class="single-product-item">
     <div class="product-image">
-      <a href="#"><img src="{{ asset('storage/'.$p->gambar) }}" alt="{{ $p->nama }}"></a>
+      <a href="#"><img src="{{ asset('storage/'.$p->gambar) }}" alt="{{ $p->nama_produk }}"></a>
     </div>
-    <h3>{{ $p->nama }}</h3>
+    
+    {{-- ⭐ PERBAIKAN 1: Gunakan NAMA PRODUK di H3 ⭐ --}}
+    <h3>{{ $p->nama_produk }}</h3>
+    
+    {{-- ⭐ PERBAIKAN 2: Hapus atau Sembunyikan Deskripsi ⭐
     <p class="product-description">{{ $p->deskripsi }}</p>
+    --}}
+    
     <p class="product-price">Rp {{ number_format($p->harga,0,',','.') }}</p>
+    
     <form action="{{ route('cart.add', $p->id) }}" method="POST" class="add-to-cart-form">
     @csrf
     <button type="submit" class="cart-btn-custom">
